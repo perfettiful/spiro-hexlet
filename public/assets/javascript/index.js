6beff695a37b061ff6d71389f870989e4dc9057f
Nate <nathanperfetti@gmail.com>
2018-09-04 20:10:07 -0500

    Basic translate animation on Konva layer with major circle

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -23,3 +23,27 @@ layer.add(circle);
 
 // add the layer to the stage
 stage.add(layer);
+
+var hexagon = new Konva.RegularPolygon({
+    x: stage.getWidth() / 2,
+    y: stage.getHeight() / 2,
+    sides: 6,
+    radius: 20,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4
+});
+
+layer.add(hexagon);
+stage.add(layer);
+
+var amplitude = 100;
+var period = 2000;
+// in ms
+var centerX = stage.getWidth() / 2;
+
+var anim = new Konva.Animation(function(frame) {
+    hexagon.setX(amplitude * Math.sin(frame.time * 2 * Math.PI / period) + centerX);
+}, layer);
+
+anim.start();
